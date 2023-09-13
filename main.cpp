91d6d7e850d5bff29aa5260e50449993d996aea9
IglooSyrup <darckuvina@gmail.com>
2023-09-13 12:11:08 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world! this is an equivalent of system.out.println in java"
+    ;
+//    << std::endl;
+//   dunno what that does for now
+    return 0;
+}
